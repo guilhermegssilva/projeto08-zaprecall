1c5b5f492d5f531ca31f269c2173849fea6c56b4
Guilherme Silva <102394141+guilhermegssilva@users.noreply.github.com>
2022-05-16 18:04:08 -0300

    Mudanças finais

--- src/Card.js
+++ src/Card.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState } from "react"; 
 
 export default function Card(props) {
     const {num, question, answer, completed, setCompleted, iconList, setIconList, setWrongAnswer} = props
